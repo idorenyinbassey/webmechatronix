bf3f45d02e64d6afcfcac7942038ddc380b14e1c
idorenyinbassey <idorenyinbassey@gmail.com>
2016-10-05 00:59:19 +0100

    New project

---
+++ site/script/menu.js
@@ -0,0 +1,6 @@
+function w3_open() {
+    document.getElementsByClassName("w3-sidenav")[0].style.display = "block";
+}
+function w3_close() {
+    document.getElementsByClassName("w3-sidenav")[0].style.display = "none";
+}
